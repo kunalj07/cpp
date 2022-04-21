71da3589e405bee304062d504213caefb0cad63f
kunalj07 <100987092+kunalj07@users.noreply.github.com>
2022-04-21 11:37:16 +0530

    Create Rollno.cpp

---
+++ Rollno.cpp
@@ -0,0 +1,35 @@
+ 
+
+ #include<iostream.h> 
+
+ #include<conio.h> 
+
+ void main() 
+
+ { 
+
+ int roll; 
+
+ char name[20]; 
+
+ clrscr(); 
+
+ cout<<"enter your name"; 
+
+ cin>>name; 
+
+  
+
+ cout<<"enter you roll no"; 
+
+ cin>>roll; 
+
+  
+
+ cout<<"name  is"<<name<<endl; 
+
+ cout<<"roll no is"<<roll<<endl; 
+
+ getch(); 
+
+ }
