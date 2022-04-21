7c191d734392f4f0658430ccecd599247c145337
kunalj07 <100987092+kunalj07@users.noreply.github.com>
2022-04-21 11:25:10 +0530

    Create Des.cpp

---
+++ Des.cpp
@@ -0,0 +1,45 @@
+ 
+
+  
+
+ //program to demonstrate destructor. 
+
+ #include<iostream.h> 
+
+ #include<conio.h> 
+
+ class demo 
+
+ { 
+
+         public: 
+
+  
+
+               ~demo(); 
+
+ }; 
+
+  
+
+ demo::~demo() 
+
+ { 
+
+ cout<<"destructor called"<<endl; 
+
+ } 
+
+ void main() 
+
+ { 
+
+         clrscr(); 
+
+         demo d; 
+
+  
+
+         getch(); 
+
+ }
